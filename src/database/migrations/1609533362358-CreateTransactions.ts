707fc7f9f3a3160f76b230abbeb1300b6007561a
Vítor Marco <vitoradrianomarco@gmail.com>
2021-01-01 18:11:42 -0300

    modificando update para updated

--- src/database/migrations/1609533362358-CreateTransactions.ts
+++ src/database/migrations/1609533362358-CreateTransactions.ts
@@ -34,7 +34,7 @@ export default class CreateTransactions1609533362358
             default: 'now()',
           },
           {
-            name: 'update_at',
+            name: 'updated_at',
             type: 'timestamp',
             default: 'now()',
           },
